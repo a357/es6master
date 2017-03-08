84aed4d7b905e19c31c4831cd1099422f60b9ad9
object <maoarsen@gmail.com>
2017-03-09 00:47:54 +0200

    Update revinitialize.js

--- js/revinitialize.js
+++ js/revinitialize.js
@@ -14,14 +14,14 @@ Reveal.initialize({
   //control slide from another dev
   multiplex: {
     // Example values. To generate your own, see the socket.io server instructions.
-    secret: null, // Obtained from the socket.io server. Gives this (the master) control of the presentation
+    secret: '14837127463157141816', // Obtained from the socket.io server. Gives this (the master) control of the presentation
     id: 'cd4c1b33a6688b2c', // Obtained from socket.io server
     url: 'https://reveal-js-multiplex-ccjbegmaii.now.sh' // Location of socket.io server
   },
   // More info https://github.com/hakimel/reveal.js#dependencies
   dependencies: [
     { src: '//cdn.socket.io/socket.io-1.3.5.js', async: true },
-    { src: 'plugin/multiplex/client.js', async: true },
+    { src: 'plugin/multiplex/master.js', async: true },
     { src: 'plugin/markdown/marked.js' },
     { src: 'plugin/markdown/markdown.js' },
     { src: 'plugin/notes/notes.js', async: true },
